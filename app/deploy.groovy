899527e97a322002b80a50253143ff69aa4233bc
hopks016 <steven.r.hopkins.-nd@disney.com>
2018-01-30 22:55:32 -0700

    ll

---
+++ app/deploy.groovy
@@ -0,0 +1,19 @@
+#!/groovy
+
+node('ec2') {
+    withCredentials([string(credentialsId: "${NAMESPACE}-token", variable: 'TOKEN')]) {
+
+        def kubectl = """docker run --rm lachlanevenson/k8s-kubectl:v1.9.2 \
+                --namespace=${NAMESPACE} \
+                --server=${SERVER} \
+                --token=${TOKEN} \
+                --insecure-skip-tls-verify=true"""
+
+        sh "${kubectl} get -o wide deployment ${DEPLOYMENT}"
+        sh "${kubectl} set image deployment/${DEPLOYMENT} ${DEPLOYMENT}=${IMAGE}"
+
+        sh "for _ in {1..5}; do ${kubectl} rollout status --watch=true deployment/${DEPLOYMENT} || : && break; done"
+
+        sh "${kubectl} get -o wide deployment ${DEPLOYMENT}"
+    }
+}
